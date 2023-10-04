25143f2f4d358293ccb613076c0a2829cb8b73a2
Coco-ha <135057744+Coco-ha@users.noreply.github.com>
2023-10-04 13:41:03 -0500

    finished trees

--- tree.js
+++ tree.js
@@ -14,8 +14,8 @@ class TreeNode {
     //base case = node with no children
     let sum = this.val || 0;
 
-    for (let child of this.children){
-      sum += child.sumValues()
+    for (let child of this.children) {
+      sum += child.sumValues();
     }
 
     return sum;
@@ -25,14 +25,33 @@ class TreeNode {
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
   countEvens() {
+    let count = 0;
 
+    for (let child of this.children) {
+      count += child.countEvens();
+
+    }
+    if (this.val % 2 === 0) {
+      return count + 1;
+    } else {
+      return count;
+    }
   }
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
    * its children, return a count of the number of nodes whose value is greater
    * than lowerBound. */
-  numGreater(lowerBound){
+  numGreater(lowerBound) {
+    let count = 0;
 
+    for (let child of this.children) {
+      count += child.numGreater(lowerBound);
+    }
+    if (this.val > lowerBound) {
+      return count + 1;
+    } else {
+      return count;
+    }
   }
 }
 
@@ -43,19 +62,25 @@ class Tree {
 
   /** sumValues(): add up all values in the tree. */
   sumValues() {
-    if (this.root) return this.root.sumValues()
+    if (this.root) return this.root.sumValues();
     return 0;
   }
 
   /** countEvens(): count all nodes in the tree that have even values. */
   countEvens() {
-
+    if (this.root === null) {
+      return 0;
+    }
+    return this.root.countEvens();
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
   numGreater(lowerBound) {
-
+    if (this.root === null) {
+      return 0;
+    }
+    return this.root.numGreater(lowerBound);
   }
 }
 
